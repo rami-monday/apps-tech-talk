4a21a607e90f58f146764d7867b6403f84f107c2
rami-monday <rami@monday.com>
2023-06-19 08:48:22 +0300

    loading MF

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import styles from "./App.module.scss";
+import { useState } from "react";
+import { Button, Loader } from "monday-ui-react-core";
+import 'monday-ui-react-core'
 
+
+const data = { entities: {} };
+const baseURL = "http://localhost:3002";
 function App() {
+  const [mfInterface, setMfInterface] = useState();
+  const [assetsManifest, setAssetsManifest] = useState();
+  const ref = useRef(null);
+
+  const loadAssetsManifest = async () => {
+    const response = await fetch(`${baseURL}/asset-manifest.json`);
+    const json = await response.json();
+    setAssetsManifest(json);
+  };
+
+  useEffect(() => {
+    loadAssetsManifest();
+  }, []);
+
+  useEffect(() => {
+    if (!assetsManifest) return;
+
+    const script = document.createElement("script");
+
+    const src = assetsManifest?.entrypoints?.find((entrypoint) =>
+      entrypoint.endsWith(".js")
+    );
+    const [mfUuid] = src.split(".");
+
+    script.id = "MF-SCRIPT";
+    script.src = `${baseURL}/${src}`;
+    script.onload = () => {
+      setMfInterface(window[mfUuid]);
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [assetsManifest]);
+
+  if (mfInterface && ref.current) {
+    mfInterface.render(ref.current, {
+      data,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.mainContainer}>
+      <div className={styles.header}>
+        this is not MF
+        <Button>click here</Button>
+      </div>
+      <div className={styles.mfWrapper} ref={ref}>
+        <Loader size={Loader.sizes.LARGE} />
+      </div>
     </div>
   );
 }
